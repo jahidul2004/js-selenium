1c7f06a5e1a175f82075e86f773f6b89b784cf9f
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-22 18:08:47 +0600

    Selenium JS Practice-1 (Selenium Setup).

---
+++ practice1.js
@@ -0,0 +1,15 @@
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
+
+(async function example() {
+    let driver = await new Builder().forBrowser(Browser.EDGE).build();
+
+    try {
+        await driver.get("http://www.bing.com/ncr");
+        await driver
+            .findElement(By.name("q"))
+            .sendKeys("webdriver", Key.RETURN);
+        await driver.wait(until.titleIs("webdriver - Google Search"), 5000);
+    } finally {
+        await driver.quit();
+    }
+})();
